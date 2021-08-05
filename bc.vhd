662de5d5c2c71fd57562094f33a6bed5c4f29c32
vishmaria <maria.fernandabt99@gmail.com>
2021-08-05 19:33:30 -0300

    fsm do bloco de cotrole

--- bc.vhd
+++ bc.vhd
@@ -11,7 +11,7 @@ end bc;
 
 architecture bhv of bc is
     type state_type is (S0, S1, S2, S3, S4, S5);
-    signal state: state_type;
+    signal state, next_state: state_type;
 
 begin
 
@@ -22,10 +22,65 @@ begin
         elsif (clock'event and clock= '1') then
             case state is
                 when S0 =>
+                    pronto <= '0';
+                    ini <= '0'; 
+                    CA <= '0';
+                    dec <= '0';
+                    CP <= '0'; 
                     if inicio='1' then
-                        state <= S1;
+                        next_state <= S1;
                     else
-                        state <= S0;
+                        next_state <= S0;
                     end if;
+                
+                when S1 =>
+                    pronto <= '0';
+                    ini <= '1'; 
+                    CA <= '1';
+                    dec <= '0';
+                    CP <= '0'; 
+                    next_state <= S2;
+
+                when S2 =>
+                    pronto <= '0';
+                    ini <= '1'; 
+                    CA <= '1';
+                    dec <= '0';
+                    CP <= '0'; 
+                   if Az ='0' and Bz='0' then
+                        next_state <= S3;
+                    elsif Az='1' or Bz='1'
+                        next_state <= S5;
+                    else
+                        next_state <= S2;
+                    end if;
+
+                when S3 =>
+                    pronto <= '0';
+                    ini <= '1'; 
+                    CA <= '1';
+                    dec <= '0';
+                    CP <= '1'; 
+                    next_state <= S4;
+
+                when S4 =>
+                    pronto <= '0';
+                    ini <= '1'; 
+                    CA <= '1';
+                    dec <= '1';
+                    CP <= '1'; 
+                    next_state <= S5;
+
+                when S5 =>
+                    pronto <= '1';
+                    ini <= '1'; 
+                    CA <= '1';
+                    dec <= '1';
+                    CP <= '1'; 
+                    next_state <= S0;
+            end case;
+    end process;
+end bhv;
+
 
 
